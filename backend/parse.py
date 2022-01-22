35c565fe5810624f66f2ad3a886ff6109a2f4e52
IsaacStephan317 <70538305+IsaacStephan317@users.noreply.github.com>
2022-01-22 11:57:34 -0500

    Updated parse file script to get desired values and store them in newFormat.json

--- backend/parse.py
+++ backend/parse.py
@@ -1,10 +1,31 @@
-import json
+import json, datetime, random
+from tokenize import Name
+from unicodedata import name
 
-fileOpen = open(dining.json)
+fileOpen = open("dining.json")
 
 diningData = json.load(fileOpen)
 
-for foodItem in diningData['emp_details']:
-    print(foodItem)
+time = datetime.datetime.now()
+ftime = time.strftime("%m-%d-%Y")
+
+listOfDicts = []
+#Wiley
+
+court = "Wiley"
+for meal in diningData:
+    mealTime = meal['Name']
+    for station in meal['Stations']:
+        for food in station['Items']:
+            foodName = food['Name']
+            rating = random.uniform(1.0, 5.0)
+            rating = round(rating, 1)
+            listOfDicts.append(dict(datetime = ftime, court = court, food = foodName, mealtime = mealTime, ratings = rating, picture = ""))
+            
+
+with open("newFormat.json", "w") as outfile:
+    json.dump(listOfDicts, outfile)            
+
+
 
 fileOpen.close()
