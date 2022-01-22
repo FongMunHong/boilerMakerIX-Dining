54de718ab73dfa62924ed32866d1c79fc4d7ddac
Jonathan Maskew <jmaskew@purdue.edu>
2022-01-22 14:42:55 -0500

    created home page stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import HomepageDataHandler from './homepageDataHandler'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    
+    <HomepageDataHandler />
   );
 }
 
