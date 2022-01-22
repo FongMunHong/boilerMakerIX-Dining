d77419bbca8efdadd3fe312dd328aef33bb6b4f0
Jonathan Maskew <jmaskew@purdue.edu>
2022-01-22 17:53:18 -0500

    connected to test db

--- frontend/src/components/fullMenuItem.jsx
+++ frontend/src/components/fullMenuItem.jsx
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
-import Button from 'react-bootstrap';
-
-
-
+import React, { Component } from "react";
+import Button from "react-bootstrap";
 
 class FullMenuItem extends React.Component {
+  /*
     styles = {
-        marginTop: 50,
-        marginLeft: 50,
-        marginRight: 50
-    }
-    state = {};
-    render() { 
-        return(
-            <React.Fragment>
-                <div class="card" style={this.styles}>
-                <img src="..." class="food-img" alt="img-of-food"/>
-                    <div class="card-body">
-                        <h5 class="food-name">Food Name</h5>
-                        <p class="food-rating">This will be some sort of rating thing, not a p tag I hope</p>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+    marginTop: 50,
+    marginLeft: 50,
+    marginRight: 50,
+  };
+  */
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div class="card">
+          <img src="..." class="food-img" alt="img-of-food" />
+          <div class="card-body">
+            <h5 class="food-name">Food Name</h5>
+            <p class="food-rating">
+              This will be some sort of rating thing, not a p tag I hope
+            </p>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="..." class="card-img-start" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default FullMenuItem;
--- frontend/src/components/homepageFeatures.jsx
+++ frontend/src/components/homepageFeatures.jsx
@@ -15,12 +15,11 @@ import FoodInfo from "./foodInfo";
 //Arrow icon designed by Rainbow Designs (supposed to attribute creators from the site I downloaded it from)
 
 class HomepageFeatures extends React.Component {
-  state = {};
   render() {
     return (
       <React.Fragment>
         <div
-          className="card shadow mb-3 ms-3 me-3 p-3 bg-body"
+          className="card shadow mb-3 ms-3 me-3 p-3"
           style={{ borderRadius: "30px" }}
         >
           <div className="card-body">
@@ -48,7 +47,7 @@ class HomepageFeatures extends React.Component {
                     <div className="card-body">
                       <img
                         src={"./right-arrow.png"}
-                        class="card-img-top"
+                        className="card-img-top"
                         alt="..."
                       />
                       <h6 className="card-text">Full Menu</h6>
