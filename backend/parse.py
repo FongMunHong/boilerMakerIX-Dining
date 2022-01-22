2d9253e0a63ac8a78bfbc53f3297edbac4552216
IsaacStephan317 <70538305+IsaacStephan317@users.noreply.github.com>
2022-01-22 12:02:37 -0500

    Updated parse file script to get items from each dining court not just Wiley and store them in newFormat.json

--- backend/parse.py
+++ backend/parse.py
@@ -11,16 +11,19 @@ ftime = time.strftime("%m-%d-%Y")
 
 listOfDicts = []
 #Wiley
+diningCourts = ["Wiley", "Windsor", "Ford", "Earhart", "Hillenbrand"]
 
-court = "Wiley"
-for meal in diningData:
-    mealTime = meal['Name']
-    for station in meal['Stations']:
-        for food in station['Items']:
-            foodName = food['Name']
-            rating = random.uniform(1.0, 5.0)
-            rating = round(rating, 1)
-            listOfDicts.append(dict(datetime = ftime, court = court, food = foodName, mealtime = mealTime, ratings = rating, picture = ""))
+for specificCourt in diningCourts:
+    court = specificCourt
+    for meal in diningData:
+        mealTime = meal['Name']
+        for station in meal['Stations']:
+            for food in station['Items']:
+                foodName = food['Name']
+                rating = random.uniform(1.0, 5.0)
+                rating = round(rating, 1)
+                listOfDicts.append(dict(datetime = ftime, court = court, food = foodName, mealtime = mealTime, ratings = rating, picture = ""))
+
             
 
 with open("newFormat.json", "w") as outfile:
