e1a7680ab3d03c66d001cbc35f4d9216c0a8f135
IsaacStephan317 <70538305+IsaacStephan317@users.noreply.github.com>
2022-01-22 13:20:04 -0500

    Updated parse file script to get items from each dining court and gathering place, as well as create a folder to store our json files. The Json file writing now occurs in diningRequest.py

--- backend/parse.py
+++ backend/parse.py
@@ -2,19 +2,13 @@ import json, datetime, random
 from tokenize import Name
 from unicodedata import name
 
-fileOpen = open("dining.json")
+def simplify_data(location, masterList):
+    time = datetime.datetime.now()
+    ftime = time.strftime("%m-%d-%Y")
 
-diningData = json.load(fileOpen)
-
-time = datetime.datetime.now()
-ftime = time.strftime("%m-%d-%Y")
-
-listOfDicts = []
-#Wiley
-diningCourts = ["Wiley", "Windsor", "Ford", "Earhart", "Hillenbrand"]
-
-for specificCourt in diningCourts:
-    court = specificCourt
+    court = location
+    fileOpen = open("backend/dataFiles/dining"+ location + ".json")
+    diningData = json.load(fileOpen)
     for meal in diningData:
         mealTime = meal['Name']
         for station in meal['Stations']:
@@ -22,13 +16,7 @@ for specificCourt in diningCourts:
                 foodName = food['Name']
                 rating = random.uniform(1.0, 5.0)
                 rating = round(rating, 1)
-                listOfDicts.append(dict(datetime = ftime, court = court, food = foodName, mealtime = mealTime, ratings = rating, picture = ""))
+                masterList.append(dict(datetime = ftime, court = court, food = foodName, mealtime = mealTime, ratings = rating, picture = ""))
 
-            
-
-with open("newFormat.json", "w") as outfile:
-    json.dump(listOfDicts, outfile)            
-
-
-
-fileOpen.close()
+    fileOpen.close()
+    
